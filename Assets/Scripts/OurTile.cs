61e683f681a8686d5a8f221e74e68301a16dc3b6
SiiMeR <siimraudsepp@msn.com>
2020-04-04 15:09:08 +0300

    Add Tile position

---
+++ Assets/Scripts/OurTile.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OurTile : MonoBehaviour
+{
+    public const int monthLength = 30;
+    
+    public bool isRiver;
+    public bool isRoad;
+
+    public float groundPollution = 0.05f;
+
+    public int grass = 1000;
+    public int rabbits = 100;
+    public int foxes = 10;
+
+    public TileType type = TileType.GRASS;
+
+    public void SetData(bool isRiver, bool isRoad, float groundPollution, int grasss, int rabbits, int foxes, TileType type)
+    {
+        this.isRiver = isRiver;
+        this.isRoad = isRoad;
+        this.groundPollution = groundPollution;
+        this.grass = grasss;
+        this.foxes = foxes;
+        this.type = type;
+    }
+    
+    
+}
