164570a435f212a87281d0a76bf9c3e50dd5cfd6
qristjan <kristjanps96@gmail.com>
2020-04-04 17:24:44 +0300

    mfmf

--- Assets/Scripts/OurTile.cs
+++ Assets/Scripts/OurTile.cs
@@ -14,15 +14,17 @@ public class OurTile : MonoBehaviour
     public int rabbits = 100;
     public int foxes = 10;
     public Vector3Int positionInTilemap;
+    public Tile tile;
 
     public TileType type = TileType.GRASS;
 
-    public void SetData(bool isRiver, bool isRoad, float groundPollution, int grasss, int rabbits, int foxes, TileType type)
+    public void SetData(bool isRiver, bool isRoad, float groundPollution, int grass, int rabbits, int foxes, TileType type)
     {
         this.isRiver = isRiver;
         this.isRoad = isRoad;
         this.groundPollution = groundPollution;
-        this.grass = grasss;
+        this.grass = grass;
+        this.rabbits = rabbits;
         this.foxes = foxes;
         this.type = type;
     }
