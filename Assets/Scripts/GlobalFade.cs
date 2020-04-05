7eafdcca6648ef00ee20545747e11b0fe445dde7
SiiMeR <siimraudsepp@msn.com>
2020-04-05 12:02:05 +0300

    Fadeing

--- Assets/Scripts/GlobalFade.cs
+++ Assets/Scripts/GlobalFade.cs
@@ -1,22 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
-using UnityEngine;
+﻿using DG.Tweening;
 using UnityEngine.UI;
 
 public class GlobalFade : Singleton<GlobalFade>
 {
     public Image Fade;
     
-    public Tween FadeOut(float seconds = 1f)
+    public Tween FadeOut(float seconds = .7f)
     {
         return Fade.DOFade(1.0f, seconds)
             .SetEase(Ease.OutExpo);
     }
     
-    public Tween FadeIn(float seconds = 1f)
+    public Tween FadeIn(float seconds = .7f)
     {
         return Fade.DOFade(0.0f, seconds)
-            .SetEase(Ease.InExpo);
+            .SetEase(Ease.InQuint);
     }
 }
